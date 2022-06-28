9e85ca6ae5ec276899e44c97df101c245d70dd81
Bereket16 <bereketabebe392@gmail.com>
2022-06-28 19:10:33 -0300

    task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -42,12 +42,10 @@ listint_t *insert_node(listint_t **head, int number)
 			temp = current->next;
 			current->next = new_node;
 			new_node->next = temp;
-d = new_node;
-	return (*head);
-}		}
+		}
 		return (*head);
 	}
 	new_node->next = NULL;
-	*head =new_node;
-        return (*head);
-[[B}
+	*head = new_node;
+	return (*head);
+}
